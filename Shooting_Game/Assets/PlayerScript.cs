2cacc5eea97c01fe4ee00044e624ab2841db0419
savemefromhell <86611084+savemefromhell@users.noreply.github.com>
2022-06-21 16:33:40 +0900

    1.0.2

---
+++ Shooting_Game/Assets/PlayerScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        this.transform.Translate(new Vector3(h,v, 0));
+        
+    }
+}
