2f0d73f8ee49b93bbfb11e54b0b52b27aa145c8c
savemefromhell <86611084+savemefromhell@users.noreply.github.com>
2022-06-22 12:53:28 +0900

    1.0.1
    플레이어 움직이는 코드

--- Shooting_Game/Assets/PLayer.cs
+++ Shooting_Game/Assets/PLayer.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PLayerScript : MonoBehaviour
+public class PLayer : MonoBehaviour
 {
     Rigidbody2D rigid2D;
-
+    //float maxSpeed = 2.0f;
     float Force = 30.0f;
 
     // Start is called before the first frame update
@@ -17,17 +17,18 @@ public class PLayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int key = 0;
-        int Rkey = 0;
+        int key = 0,Rkey=0;
         if (Input.GetKey(KeyCode.UpArrow)) key = 1;
         if (Input.GetKey(KeyCode.DownArrow)) key = -1;
         if (Input.GetKey(KeyCode.RightArrow)) Rkey = 1;
         if (Input.GetKey(KeyCode.LeftArrow)) Rkey = -1;
 
+        //float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
 
-         this.rigid2D.AddForce(transform.up * key * Force);
-         this.rigid2D.AddForce(transform.right * Rkey * Force);
+        this.rigid2D.AddForce(transform.up * key * Force);
+        this.rigid2D.AddForce(transform.right * Rkey * Force);
 
+       
     }
 }
